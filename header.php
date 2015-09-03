feb988349f10b6379b11648eb60796c89765fa59
JeppeSigaard <jeppe@smatmonkey.dk>
2015-09-03 11:53:31 +0200

    fjernet header

--- header.php
+++ header.php
@@ -8,11 +8,6 @@
 </head>
 <body <?php body_class(); ?>>
     <div class="splasher">
-        <header class="splasher-site-header">
-            <div class="logo">
-                <img src="<?php echo get_template_directory_uri() ?>/libs/img/logo.svg">
-            </div>
-        </header>
         <section class="not-ready">
             <article class="inner">
                 <h1>Hjemmesiden er under udarbejdelse</h1>
